c61412fd5ea5de522f7dc6ef213be61bcb432f24
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-09 17:07:11 +0530

    Initial Commit

---
+++ Weather app (iOS)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather app (iOS)
+//
+//  Created by Meet Balani on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather app (iOS)/Weather_app__iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_app__iOS_App.swift
+//  Weather app (iOS)
+//
+//  Created by Meet Balani on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_app__iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
